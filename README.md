5f2c6ff01e88fab774411a45423e58d7c838bf1b
Martin Mois <martin.mois@googlemail.com>
2017-01-26 21:43:56 +0100

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # amazonrekognition
-Sample client for Amazon's rekognition service
+
+Sample client for Amazon's rekognition service.
+
+Requires:
+
+* JDK >= 1.8
+* maven >= 3.x
+
+Build with:
+
+	mvn clean install
+	
+Run with:
+
+	java -jar target/amazon-rekognition-0.0.1-SNAPSHOT-jar-with-dependencies.jar <path-to-image-folder>
+	
+Sends all images within the folder `<path-to-image-folder>` to Amazon's rekognition service to detect faces and creates 
+for each image a new image with file ending `_withBox.jpg` with the returned bounding box drawn as red rectangle.
+ 
+The credentials for AWS can be provided using the file ` ~/.aws/credentials` with a default section:
+ 
+	[default]
+	aws_access_key_id=<your key>
+	aws_secret_access_key=<your secrect key>
+
---
+++ src/main/java/de/martinmois/amazon/rekognition/DrawBoundingBoxes.java
@@ -0,0 +1,151 @@
+package de.martinmois.amazon.rekognition;
+
+import com.amazonaws.AmazonClientException;
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.profile.ProfileCredentialsProvider;
+import com.amazonaws.services.rekognition.AmazonRekognitionClient;
+import com.amazonaws.services.rekognition.model.*;
+import com.amazonaws.services.rekognition.model.Image;
+import org.imgscalr.Scalr;
+
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class DrawBoundingBoxes {
+    private static final Logger LOGGER = Logger.getLogger(DrawBoundingBoxes.class.getName());
+
+    public static void main(String[] args) throws Exception {
+
+        if (args.length < 1) {
+            System.out.println("Please provide the following arguments: <path-to-image-folder>");
+            return;
+        }
+        String dirArg = args[0];
+        Path dirPath = Paths.get(dirArg);
+        if (!Files.exists(dirPath)) {
+            System.err.println("The provided directory does not exist.");
+            return;
+        }
+        if (!Files.isDirectory(dirPath)) {
+            System.err.println("The provided file is not a directory.");
+            return;
+        }
+
+        AWSCredentials credentials;
+        try {
+            credentials = new ProfileCredentialsProvider("default").getCredentials();
+        } catch (Exception e) {
+            throw new AmazonClientException("Cannot load the credentials from the credential profiles file. "
+                    + "Please make sure that your credentials file is at the correct "
+                    + "location (/Users/<userid>/.aws/credentials), and is in a valid format.", e);
+        }
+        AmazonRekognitionClient rekognitionClient = new AmazonRekognitionClient(credentials)
+                .withEndpoint("rekognition.eu-west-1.amazonaws.com");
+        rekognitionClient.setSignerRegionOverride("eu-west-1");
+
+        Files.walkFileTree(dirPath, new FileVisitor<Path>() {
+            @Override
+            public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
+                return FileVisitResult.CONTINUE;
+            }
+
+            @Override
+            public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+                detectFaces(rekognitionClient, file);
+                return FileVisitResult.CONTINUE;
+            }
+
+            @Override
+            public FileVisitResult visitFileFailed(Path file, IOException exc) throws IOException {
+                return FileVisitResult.CONTINUE;
+            }
+
+            @Override
+            public FileVisitResult postVisitDirectory(Path dir, IOException exc) throws IOException {
+                return FileVisitResult.CONTINUE;
+            }
+        });
+    }
+
+    private static void detectFaces(AmazonRekognitionClient rekognitionClient, Path imagePath) throws IOException {
+        try {
+            if (imagePath.toString().endsWith("_withBox.jpg")) {
+                return;
+            }
+            LOGGER.info("Scaling image " + imagePath);
+            byte[] scaledImg = scaleImage(imagePath);
+            LOGGER.info("Detecting faces on image " + imagePath);
+            DetectFacesRequest request = new DetectFacesRequest()
+                    .withImage(new Image().withBytes(ByteBuffer.wrap(scaledImg)));
+            DetectFacesResult result = rekognitionClient.detectFaces(request);
+            BufferedImage bufferedImage = ImageIO.read(new ByteArrayInputStream(scaledImg));
+            Graphics2D graphics = bufferedImage.createGraphics();
+            graphics.setColor(Color.red);
+            graphics.setStroke(new BasicStroke(20.0f, BasicStroke.CAP_BUTT, BasicStroke.JOIN_BEVEL, 10.0f));
+            List<FaceDetail> faceDetails = result.getFaceDetails();
+            if (!faceDetails.isEmpty()) {
+                for (FaceDetail faceDetail : faceDetails) {
+                    BoundingBox boundingBox = faceDetail.getBoundingBox();
+                    int left = (int) (boundingBox.getLeft() * bufferedImage.getWidth());
+                    int top = (int) (boundingBox.getTop() * bufferedImage.getHeight());
+                    int width = (int) (boundingBox.getWidth() * bufferedImage.getWidth());
+                    int height = (int) (boundingBox.getHeight() * bufferedImage.getHeight());
+                    LOGGER.info("BoundingBox: " + left + "/" + top + " " + width + "/" + height);
+                    graphics.drawRect(left, top, width, height);
+                    Gender gender = faceDetail.getGender();
+                    if (gender != null) {
+                        LOGGER.info("Gender: " + gender.getValue() + " " + gender.getConfidence());
+                    }
+                }
+            } else {
+                LOGGER.info("No face details returned for " + imagePath);
+            }
+            graphics.dispose();
+            Path newFilePath = Paths.get(imagePath.toString() + "_withBox.jpg");
+            ImageIO.write(bufferedImage, "jpg", newFilePath.toFile());
+        } catch (AmazonRekognitionException e) {
+            LOGGER.log(Level.SEVERE, "Failed to detect faces on image '" + imagePath + "': " + e.getLocalizedMessage());
+        }
+    }
+
+    private static byte[] scaleImage(Path imagePath) throws IOException {
+        BufferedImage bufferedImage = ImageIO.read(imagePath.toFile());
+        int w = bufferedImage.getWidth();
+        int h = bufferedImage.getHeight();
+        boolean landscape = true;
+        if (w < h) {
+            landscape = false;
+        }
+        boolean needScaling = false;
+        if (landscape) {
+            if (w > 1920) {
+                needScaling = true;
+            }
+        } else {
+            if (h > 1920) {
+                needScaling = false;
+            }
+        }
+        BufferedImage scaled = bufferedImage;
+        if (needScaling) {
+            if (landscape) {
+                scaled = Scalr.resize(bufferedImage, Scalr.Method.BALANCED, 1920, 1080);
+            } else {
+                scaled = Scalr.resize(bufferedImage, Scalr.Method.BALANCED, 1080, 1920);
+            }
+        }
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        ImageIO.write(scaled, "jpg", baos);
+        return baos.toByteArray();
+    }
+}
